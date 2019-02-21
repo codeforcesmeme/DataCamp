ad392d3be4bd2fed8b0b093ee091bcddcc2eb3f5
codeforcesmeme <45147859+codeforcesmeme@users.noreply.github.com>
2019-02-21 03:36:32 +0200

    Update 09-counting-usa-vs-ussr-cold-war.py

--- 12-manipulating-dataframes-with-pandas/05-putting-it-all-together/09-counting-usa-vs-ussr-cold-war.py
+++ 12-manipulating-dataframes-with-pandas/05-putting-it-all-together/09-counting-usa-vs-ussr-cold-war.py
@@ -20,11 +20,11 @@ Create the Series most_medals by applying the .idxmax() method to cold_war_usa_u
 # Create the pivot table: medals_won_by_country
 medals_won_by_country = medals.pivot_table(index='Edition', columns='NOC', values='Athlete', aggfunc='count')
 
-# Slice medals_won_by_country: cold_war_usa_usr_medals
-cold_war_usa_usr_medals = medals_won_by_country.loc[1952:1988, ['USA','URS']]
+# Slice medals_won_by_country: cold_war_usa_urs_medals
+cold_war_usa_urs_medals = medals_won_by_country.loc[1952:1988, ['USA','URS']]
 
-# Create most_medals 
-most_medals = cold_war_usa_usr_medals.idxmax(axis='columns')
+# Create most_medals
+most_medals = cold_war_usa_urs_medals.idxmax(axis='columns')
 
 # Print most_medals.value_counts()
 print(most_medals.value_counts())
